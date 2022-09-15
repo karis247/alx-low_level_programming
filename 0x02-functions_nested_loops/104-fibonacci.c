c1b8e4663f7febd49811ca7444831ad4bc58484f
karis247 <chicheta440@gmail.com>
2022-09-14 21:58:58 -0700

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -50,4 +50,14 @@ int main(void)
 		tmpo = f1o + f2o + (f1 + f2) / mx;
 		f1 = f2;
 		f1o = f2o;
-…
+		f2 = tmp;
+		f2o = tmpo;
+
+		if (i != 98)
+			printf(", ");
+		else
+			printf("\n");
+		i++;
+	}
+	return (0);
+}
