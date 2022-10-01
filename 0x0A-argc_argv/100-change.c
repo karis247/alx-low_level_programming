0d01127b197bb43ff65a98cb289c78559a630a22
karis247 <chicheta440@gmail.com>
2022-09-30 18:33:28 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,80 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
-
-/**
- * main - Program that takes first two integer arguments and prints the product
- * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 1 if not enough arguments passed in, 0 otherwise
- */
-
-int main(int argc, char *argv[])
-{
-	int i, j;
-
-	if (argc == 1 || argc == 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	else
-	{
-		j = 1;
-
-		for (i = 1; i < 3; i++)
-		j *= atoi(argv[i]);
-
-		printf("%d\n", j);
-	}
-
-	return (0);
-}
-[2:14 AM, 10/1/2022] Ariselz Hair and Hair Products: #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include "main.h"
-
-/**
- * main - Program that takes in all integer arguments and returns the sum
- * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
- */
-
-int main(int argc, char *argv[])
-{
-	int i, j, length, sum;
-	char *ptr;
-
-	if (argc < 2)
-	printf("0\n");
-	else
-	{
-		sum = 0;
-		for (i = 1; i < argc; i++)
-		{
-			ptr = argv[i];
-			length = strlen(ptr);
-
-			for (j = 0; j < length; j++)
-			{
-				if (isdigit(*(ptr + j)) == 0)
-				{
-					printf("Error\n");
-					return (1);
-				}
-			}
-
-			sum += atoi(argv[i]);
-		}
-
-		printf("%d\n", sum);
-	}
-	return (0);
-}
-[2:18 AM, 10/1/2022] Ariselz Hair and Hair Products: #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -146,4 +70,4 @@ int main(int argc, char *argv[])
 	}
 
 	return (0);
-}
+
