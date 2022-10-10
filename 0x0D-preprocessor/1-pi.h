2359ed9145c89fce90d1cc17fafd331a8a36f4c7
karis247 <chicheta440@gmail.com>
2022-10-10 15:45:48 -0700

    task 1

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,13 +1,11 @@
-#include <stdio.h>
+#ifndef _PI_
 
-/**
- * main - Prints the name of the file of the program
- *        was compiled from, followed by a new line.
- * Return: Always 0.
- */
-int main(void)
-{
-	printf("%s\n", _FILE_);
+#define _PI_
 
-	return (0);
-}
+
+
+#define PI 3.14159265359
+
+
+
+#endif
