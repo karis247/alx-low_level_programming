50e85280944134370baf16bdfc41816360a83291
karis247 <chicheta440@gmail.com>
2022-10-21 22:04:40 -0700

    function pointer

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-typedef _SIZE_TYPE_ size_t;
+#ifndef _HEADER_
+#define _HEADER_
+#include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
